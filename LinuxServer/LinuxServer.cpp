47b48be6ed171dc2f2832cc55779fc2d99c0d827
Huan Xia <huxia1124@gmail.com>
2016-09-18 14:08:19 -0500

    Add some output at startup

--- LinuxServer/LinuxServer.cpp
+++ LinuxServer/LinuxServer.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	char sz[] = "Hello, World!";	//Hover mouse over "sz" while debugging to see its contents
-	cout << sz << endl;	//<================= Put a breakpoint here
+	unsigned int port = 9988;
 	
 	CAsyncServer server;
 	if (!server.CreateTcpSubServer(9988, nullptr))
 	{
+		cout << "Error: TCP Server Failed to start! Port=" << port << endl;
 		return 1;
 	}
-	
+	cout << "TCP Server started, listening on port " << port << endl;
 	server.Wait();
 	
 	return 0;
